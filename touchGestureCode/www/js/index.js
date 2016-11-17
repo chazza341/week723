9600862f690a2d346b819a9257b8e6b3b1670072
chazza341 <LESC1_15@uni.worc.ac.uk>
2016-11-17 11:40:02 +0000

    function pasted from worksheet into code

---
+++ touchGestureCode/www/js/index.js
@@ -0,0 +1,13 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+  
+	$('#tapt').on("tap",function(){
+        var r=random();
+        alert(r);
+ 	}); 
+
+});
+function random() {
+	return !Math.round(Math.random());
+}
